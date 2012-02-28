7c829028fa7fe58ba6c204547c8c219d7b77559c
torstenchr <github.com@bondechristiansen.dk>
2012-02-28 11:39:16 +0000

    * Fixed bug where field names with non-ASCII characters (and other symbols) caused A/V during load.

--- entryprocs.pas
+++ entryprocs.pas
@@ -21,6 +21,7 @@ type
   TCharArray = array of char;
 
 procedure LoadIniFile;
+function GetRandomComponentName: string;
 
 implementation
 
@@ -40,6 +41,18 @@ begin
   if not DirectoryExistsUTF8(ExtractFilePath(GetAppConfigFileUTF8(false))) then
     ForceDirectoriesUTF8(ExtractFilePath(GetAppConfigFileUTF8(false)));
   EntrySettings.IniFileName := GetAppConfigFileUTF8(false);
+end;
+
+function GetRandomComponentName: string;
+var
+  GUID: TGUID;
+begin
+  // Hack: Create a GUID to use as Component name.
+  //  - the comp. name is not used in other parts of the program anyway,
+  //  - so using GUID is a valid way to create random components names... :)
+  //  - And the chance of creating to equal component name are very-very-very unlikely.
+  CreateGUID(GUID);
+  Result := '_' + StringsReplace(GUIDToString(GUID), ['{','}','-'], ['','',''], [rfReplaceAll]);
 end;
 
 end.
