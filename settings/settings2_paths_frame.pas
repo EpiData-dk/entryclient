b4e0960028f3ef1c58f132286afc83ed0352bc59
torstenchr <github.com@bondechristiansen.dk>
2013-11-12 13:28:39 +0000

    * Merged /trunk:r325-331
    r324 | torsten | 2013-10-31 14:04:26 +0100 (Thu, 31 Oct 2013) | 2 lines
    * Backup-on-shutdown file changed to: <filename>_<date>_<cycle>.epz
    
    r328 | torsten | 2013-11-11 14:56:00 +0100 (Mon, 11 Nov 2013) | 3 lines
    * Added settings option:
    * - Show Extra field information as note.
    
    r329 | torsten | 2013-11-11 15:16:15 +0100 (Mon, 11 Nov 2013) | 2 lines
    * Added shortcut (+) to Date/Time fieldedits, such that is automatically enters current date/time.
    
    r330 | torsten | 2013-11-11 15:26:11 +0100 (Mon, 11 Nov 2013) | 2 lines
    * Added extra date/time fieldedit functionality to statuspanel.
    
    r331 | torsten | 2013-11-12 11:32:43 +0100 (Tue, 12 Nov 2013) | 3 lines
    * Added a specific Backup folder in which the user may specify the daily backups to be saved.
    * - includes a setting to make project specific sub-folders (on by default).

--- settings/settings2_paths_frame.pas
+++ settings/settings2_paths_frame.pas
@@ -13,12 +13,15 @@ type
   { TSettingsPathsFrame }
 
   TSettingsPathsFrame = class(TFrame, ISettingsFrame)
+    PerProjectChkBox: TCheckBox;
+    Label1: TLabel;
     Label17: TLabel;
     Label18: TLabel;
     Label3: TLabel;
     TutorialDirEdit: TDirectoryEdit;
     TutorialURLEdit: TEdit;
     WorkingDirEdit: TDirectoryEdit;
+    BackupFolderEdit: TDirectoryEdit;
   private
     { private declarations }
     FData: PEntrySettings;
@@ -45,6 +48,8 @@ begin
     WorkingDirEdit.Text := WorkingDirUTF8;
     TutorialDirEdit.Text := TutorialDirUTF8;
     TutorialURLEdit.Text := TutorialURLUTF8;
+    BackupFolderEdit.Text := BackupDirUTF8;
+    PerProjectChkBox.Checked := PerProjectBackup;
   end;
 end;
 
@@ -58,11 +63,15 @@ begin
       (LeftStr(UTF8LowerCase(TutorialURLEdit.Text), 7) = 'http://') or
       (LeftStr(UTF8LowerCase(TutorialURLEdit.Text), 8) = 'https://')) then exit;
 
+  if not DirectoryExistsUTF8(BackupFolderEdit.Text) then exit;
+
   With FData^ do
   begin
     WorkingDirUTF8 := WorkingDirEdit.Text;
     TutorialDirUTF8 := TutorialDirEdit.Text;
     TutorialURLUTF8 := TutorialURLEdit.Text;
+    BackupDirUTF8    := BackupFolderEdit.Text;
+    PerProjectBackup := PerProjectChkBox.Checked;
   end;
   result := true;
 end;
--- utils/documentfile_ext.pas
+++ utils/documentfile_ext.pas
@@ -19,7 +19,7 @@ type
 implementation
 
 uses
-  FileUtil;
+  LazFileUtils, settings;
 
 { TEntryDocumentFile }
 
@@ -29,15 +29,31 @@ var
   M: word;
   D: word;
   S: String;
+  Prefix: String;
+  FileNameNoExt: String;
 begin
-  if FileName = '' then exit;
+  Result := False;
+  if (not IsSaved) then exit;
+
+  Prefix := EntrySettings.BackupDirUTF8;
+  FileNameNoExt := ExtractFileNameOnly(FileName);
+  if EntrySettings.PerProjectBackup then
+    Prefix := Prefix + DirectorySeparator + FileNameNoExt + DirectorySeparator;
+
+  if not DirectoryExistsUTF8(Prefix) then
+    if not CreateDirUTF8(Prefix) then exit;
 
   DecodeDate(Now, Y, M, D);
-  S := ExtractFileNameWithoutExt(FileName) +       // FileName
+  S := Prefix + FileNameNoExt +                    // <backupdir>[/<projectfilename>]
        '_' + Format('%d-%.2d-%.2d', [Y,M,D]) +     // _<date>
        '_' + IntToStr(Document.CycleNo) +          // _<cycle>
-       '.epz';                                     // eg: test_2013-24-10_2.epz
-  DoSaveFile(S);
+       '.epz';                                     // eg:  ./backupdir/myproject/test_2013-24-10_2.epz
+
+  try
+    DoSaveFile(S);
+    Result := true;
+  finally
+  end;
 end;
 
 end.
