bbf5abd3ed6b95bccabd962404f896e9626c214f
Jamie <charlesknightsbridge@gmail.com>
2024-05-04 07:02:58 -0400

    Fix message in failed comparison
    For proper sense, "is not" added before the comparison operator.

--- src/dataform/dataform_frame.pas
+++ src/dataform/dataform_frame.pas
@@ -2672,7 +2672,7 @@ begin
     begin
       Err := Format(
         'Comparison failed:' + LineEnding +
-        '%s: %s  %s  %s: %s',
+        '%s: %s is not %s  %s: %s',
         [Field.Name, CE.Text, ComparisonTypeToString(Field.Comparison.CompareType),
          Field.Name, NewEdit.Text]);
       FieldValidateError(CE, Err);
